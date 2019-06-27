4554f119cbe557950a682c61d33b831b1a825515
EnimaTita <tabtani.m.amine@gmail.com>
2019-06-27 17:17:11 +0100

    e

--- src/main/resources/Jenkins.java
+++ src/main/resources/Jenkins.java
@@ -9,10 +9,4 @@ public class Jenkins {
 	public static void run(int i){
 	
 	}
-	public static void run2(int i){
-		System.out.println("Hello we are here");
-	}
-	public static void run2(int j, float x){
-		System.out.println();
-	}
 }
